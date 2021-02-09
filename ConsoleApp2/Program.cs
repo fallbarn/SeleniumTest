e9341601183296155268f9eaee85e874ccd378b0
Simon Ellis <fallbarn@hotmail.com>
2021-02-09 15:15:35 +0000

    test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace ConsoleApp2
 {
@@ -7,6 +9,12 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Creating Selenium driver");
+
+            IWebDriver browser = new ChromeDriver();
+
+            browser.Navigate().GoToUrl("https://google.com");
         }
     }
 }
